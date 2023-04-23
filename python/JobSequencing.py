daec053949402f4719566e9a16bbc44d9eeb6d79
Zane Falcao <falcaozane@gmail.com>
2023-04-23 11:51:43 +0530

    improved the sorting part

--- python/JobSequencing.py
+++ python/JobSequencing.py
@@ -1,25 +1,26 @@
-def printJobScheduling(arr, t):
-    n = len(arr)
-    for i in range(n):
-        for j in range(n-i-1):
-            if arr[j][2] < arr[j+1][2]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def JS(A,t):
+    n = len(A)
+    # sort in descending order of profit
+    A.sort(key=lambda x: x[2], reverse=True)
     res = [False] * t
-    job = ['-1'] * t
+    job = ['-1']*t
+    total_profit = 0
     for i in range(n):
-        for j in range(min(t-1, arr[i][1]-1), -1, -1):
+        for j in range(min(t-1,A[i][1]-1),-1,-1):
             if res[j] is False:
                 res[j] = True
-                job[j] = arr[i][0]
+                job[j] = A[i][0]
+                total_profit += A[i][2]
                 break
-    print("Maximum profit sequence of jobs is- ")
-    print(", ".join(job))
+    print("\nMax profit sequence: ")
+    print(job)
+    print("Total profit: ", total_profit)
 
-array = [
+array = [       
        ['j1', 2, 20],
        ['j2', 2, 15],
        ['j3', 1, 10],
        ['j4', 3, 5],
        ['j5', 3, 1]
        ]
-printJobScheduling(array, 3)
+JS(array, 3)
